9bd9198722f69b5f1483d4b2d4d77935ad45f750
Efrain Vergara <efrainvergara.udec@gmail.com>
2020-07-09 01:29:34 -0500

    Follows

--- controllers/follow.js
+++ controllers/follow.js
@@ -15,101 +15,140 @@ function saveFollow(req, res) {
     follow.followed = params.followed;
 
     follow.save((err, followStored) => {
-        if (err) return res.status(500).send({message: 'Error al guardar el seguimiento'});
+        if (err) return res.status(500).send({ message: 'Error al guardar el seguimiento' });
 
-        if (!followStored) return res.status(404).send({message: 'El seguimiento no se ha guardado'});
+        if (!followStored) return res.status(404).send({ message: 'El seguimiento no se ha guardado' });
 
-        return res.status(200).send({follow: followStored});
+        return res.status(200).send({ follow: followStored });
     })
 }
 
 function deleteFollow(req, res) {
     var userId = req.user.sub;
     var followId = req.params.id;
-    
-    Follow.find({'user':userId, 'followed':followId}).remove(err => {
-        if (err) return res.status(500).send({message: 'Error al dejar de seguir'});
 
-        return res.status(200).send({message: 'El follow se ha eliminado'});
+    Follow.find({ 'user': userId, 'followed': followId }).remove(err => {
+        if (err) return res.status(500).send({ message: 'Error al dejar de seguir' });
+
+        return res.status(200).send({ message: 'El follow se ha eliminado' });
     })
-    
+
 }
 
 function getFollowinguser(req, res) {
     var userId = req.user.sub;
-    
-    if(req.params.id && req.params.page){
+
+    if (req.params.id && req.params.page) {
         userId = req.params.id;
     }
 
     var page = 1;
 
-    if(req.params.page){
+    if (req.params.page) {
         page = req.params.page;
-    }else{
+    } else {
         page = req.params.id;
     }
 
     var itemsPerPage = 4;
 
-    Follow.find({user:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total) => {
-        if (err) return res.status(500).send({message: 'Error en el servidor'});
+    Follow.find({ user: userId }).populate({ path: 'followed' }).paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return res.status(500).send({ message: 'Error en el servidor' });
 
-        if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+        if (!follows) return res.status(404).send({ message: 'No estas siguiendo a ningun usuario' });
 
-        return res.status(200).send({
-            total: total,
-            pages: Math.ceil(total/itemsPerPage),
-            follows
-        })
+        followUserIds(req.user.sub).then((value) => {
+            return res.status(200).send({
+                total: total,
+                pages: Math.ceil(total / itemsPerPage),
+                follows,
+                users_following: value.following,
+                users_follow_me: value.followed,
+            });
+        });
     });
+}
+
+async function followUserIds(user_id) {
+    var following = await Follow.find({ "user": user_id }).select({ '_id': 0, '__v': 0, 'user': 0 }).exec()
+        .then((follows) => {
+            return follows;
+        });
+
+    var followed = await Follow.find({ "followed": user_id }).select({ '_id': 0, '__v': 0, 'followed': 0 }).exec()
+        .then((follows) => {
+            return follows;
+        });
+
+    //procesar following ids
+    var following_clean = [];
+
+    following.forEach((follow) => {
+        following_clean.push(follow.followed);
+    });
+
+    //procesar followed ids
+    var followed_clean = [];
+
+    followed.forEach((follow) => {
+        followed_clean.push(follow.user);
+    });
+
+    return {
+        following: following_clean,
+        followed: followed_clean
+    }
 }
 
 function getFollowedUsers(req, res) {
     var userId = req.user.sub;
-    
-    if(req.params.id && req.params.page){
+
+    if (req.params.id && req.params.page) {
         userId = req.params.id;
     }
 
     var page = 1;
 
-    if(req.params.page){
+    if (req.params.page) {
         page = req.params.page;
-    }else{
+    } else {
         page = req.params.id;
     }
 
     var itemsPerPage = 4;
 
-    Follow.find({followed:userId}).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
-        if (err) return res.status(500).send({message: 'Error en el servidor'});
+    Follow.find({ followed: userId }).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return res.status(500).send({ message: 'Error en el servidor' });
 
-        if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+        if (!follows) return res.status(404).send({ message: 'No te sigue ningun usuario' });
 
-        return res.status(200).send({
-            total: total,
-            pages: Math.ceil(total/itemsPerPage),
-            follows
-        })
+        followUserIds(req.user.sub).then((value) => {
+            return res.status(200).send({
+                total: total,
+                pages: Math.ceil(total / itemsPerPage),
+                follows,
+                users_following: value.following,
+                users_follow_me: value.followed,
+            });
+        });
     });
-    
+
 }
 
 //devolver listado de usuario
 function getMyFollows(req, res) {
     var userId = req.user.sub;
 
-    var find = Follow.find({user: userId});
+    var find = Follow.find({ user: userId });
 
-    if(req.params.followed){
-        find = Follow.find({followed:userId});
+    if (req.params.followed) {
+        find = Follow.find({ followed: userId });
     }
 
     find.populate('user followed').exec((err, follows) => {
-        if (err) return res.status(500).send({message: 'Error en el servidor'});
+        if (err) return res.status(500).send({ message: 'Error en el servidor' });
 
-        if(!follows) return res.status(404).send({message: 'No sigues a ningun usuario'});
+        if (!follows) return res.status(404).send({ message: 'No sigues a ningun usuario' });
 
         return res.status(200).send({
             follows
--- routes/publication.js
+++ routes/publication.js
@@ -16,6 +16,6 @@ api.get('/publications-user/:user/:page?', md_auth.ensureAuth, PublicationContro
 api.get('/publication/:id', md_auth.ensureAuth, PublicationController.getPublication);
 api.delete('/publication/:id', md_auth.ensureAuth, PublicationController.deletePublication);
 api.post('/upload-image-pub/:id', [md_auth.ensureAuth, md_upload], PublicationController.uploadImage);
-api.get('/get-image-pub/:imageFile?', PublicationController.getPublications);
+api.get('/get-image-pub/:imageFile', PublicationController.getImageFile);
 
 module.exports = api;
