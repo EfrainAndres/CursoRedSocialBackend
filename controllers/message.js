2277a2107836e3edfca62fad4554f157a6d53fb2
Efrain Vergara <efrainvergara.udec@gmail.com>
2020-07-11 03:06:32 -0500

    Ordering messages

--- controllers/message.js
+++ controllers/message.js
@@ -44,7 +44,7 @@ function getReceivedMessages(req, res) {
 
     var itemsPerPage = 4;
 
-    Message.find({receiver: userId}).populate('emitter', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({receiver: userId}).sort('-created_at').populate('emitter', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
         if(err) return res.status(500).send({message: 'Error en la peticion'});
         if(!messages) return res.status(404).send({message: 'No hay mensajes'});
 
@@ -66,7 +66,7 @@ function getEmmitMessages(req, res) {
 
     var itemsPerPage = 4;
 
-    Message.find({emitter: userId}).populate('emitter receiver', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({emitter: userId}).sort('-created_at').populate('emitter receiver', 'name surname nick image _id').paginate(page, itemsPerPage, (err, messages, total) => {
         if(err) return res.status(500).send({message: 'Error en la peticion'});
         if(!messages) return res.status(404).send({message: 'No hay mensajes'});
 
